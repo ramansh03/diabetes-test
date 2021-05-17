321b21b22c111f14c1172e84465d74e016e104f4
Ramansh Sangal <70139559+ramansh03@users.noreply.github.com>
2021-05-17 20:04:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,25 +2,28 @@ from flask import Flask, render_template, url_for, flash, redirect
 import joblib
 from flask import request
 import numpy as np
+import sklearn
+import pickle
+from flask_cors import cross_origin
 
 app = Flask(__name__, template_folder='templates')
 
 @app.route("/")
-
+@cross_origin()
 @app.route("/Diabetes")
 def diabetes():
     return render_template("diabetes.html")
 
-def ValuePredictor(to_predict_list, size):
+'''def ValuePredictor(to_predict_list, size):
     to_predict = np.array(to_predict_list).reshape(1,size)
     if(size==6):
         model = pickle.load(open("rf_diabetes.pkl", "rb"))
         result = model.predict(to_predict)
-    return result[0]
+    return result[0]'''
 
 @app.route('/predict', methods = ["POST"])
 def predict():
-    if request.method == "POST":
+    '''if request.method == "POST":
         to_predict_list = request.form.to_dict()
         to_predict_list = list(to_predict_list.values())
         to_predict_list = list(map(float, to_predict_list))
@@ -32,7 +35,18 @@ def predict():
         prediction = "Sorry you chances of getting the disease. Please consult the doctor immediately"
     else:
         prediction = "No need to fear. You have no dangerous symptoms of the disease"
-    return(render_template("result.html", prediction_text=prediction))       
+    return(render_template("result.html", prediction_text=prediction))'''
+    if(request.method=='POST'):
+        preg = int(request.form['pregnancies'])
+        glucose = int(request.form['glucose'])
+        bp = int(request.form['bloodpressure'])
+        st = int(request.form['skinthickness'])
+        insulin = int(request.form['insulin'])
+        bmi = float(request.form['bmi'])
+       
+        
+        data = np.array([[preg, glucose, bp, st, insulin, bmi, dpf, age]])
+        my_prediction = rf_random.predict(data)
 
 if __name__ == "__main__":
     app.run(debug=True)
