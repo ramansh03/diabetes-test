79754b0a777b0d94ae8817aec1cf0bdb5d39acec
Ramansh Sangal <70139559+ramansh03@users.noreply.github.com>
2021-05-17 19:47:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__, template_folder='templates')
 @app.route("/")
 
 @app.route("/Diabetes")
-def cancer():
+def diabetes():
     return render_template("diabetes.html")
 
 def ValuePredictor(to_predict_list, size):
